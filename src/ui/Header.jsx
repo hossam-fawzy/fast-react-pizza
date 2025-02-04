dddfd565ee6e8d3d1a977988bfe33172155dbad0
hossam-fawzy <hossamfawzy553@gmail.com>
2025-02-04 11:46:59 +0200

    tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
